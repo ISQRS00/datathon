b342163ccca84ca90f8939d3094c136769a0593a
Isaque Ramalho Dos Santos <168669233+ISQRS00@users.noreply.github.com>
2025-02-13 22:20:28 -0300

    Update Previsão_ponto_de_virada.py

--- pages/Previsão_ponto_de_virada.py
+++ pages/Previsão_ponto_de_virada.py
@@ -149,7 +149,7 @@ with abas[1]:
     num_fase = st.number_input("Número da Fase", min_value=0.0, max_value=10.0, value=5.0)
 
     # Criar um dicionário fixo para as turmas
-    dict_turma = {i: chr(65 + i) for i in range(25)}
+    dict_turma = dict_turma = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'X', 23: 'Y', 24: 'Z'}
 
     # Criar um input de texto para a letra da Turma, com padrão "A"
     letra_turma = st.text_input("Digite a letra da Turma (A a Z):", value="A", max_chars=1)
@@ -161,7 +161,7 @@ with abas[1]:
     else:
         turma_transformada = None
         valid_turma = False
-        st.error("Por favor, insira uma letra entre A e Z.")
+        st.error("Por favor, insira uma letra entre A e Z. Não existe turma com a letra W.")
 
     # Criar DataFrame com os inputs do usuário
     entrada_usuario = pd.DataFrame([[num_ian, num_ida, num_ieg, turma_transformada, num_fase]], 
